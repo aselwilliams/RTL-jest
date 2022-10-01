2928ac3f69875096e1021cb59a5beda031889f20
aselwilliams <aselwilliams22@gmail.com>
2022-10-01 14:26:51 -0500

    react testing library, jest tests login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
+  const a=2;
+  const b=4;
   return (
     <div className="App">
-      <header className="App-header">
+
+      <Login />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <ul>
+          <li>Apple</li>
+          <li>Orange</li>
+          <li>Banana</li>
+        </ul>
+        <h1 data-testid='mytestid'>Hello</h1>
+        <span title='sum'>{a+b}</span>
+      </header> */}
     </div>
   );
 }
